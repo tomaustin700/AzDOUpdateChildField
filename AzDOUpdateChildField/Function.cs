bc091b6089647adfa44c3ebad2a8ea590276e119
Tom Austin <tom@tomaustin.xyz>
2022-12-30 13:45:58 +0000

    Added logic to only update field if work item type has field

--- AzDOUpdateChildField/Function.cs
+++ AzDOUpdateChildField/Function.cs
@@ -17,6 +17,8 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.Services.WebApi.Patch.Json;
 using Microsoft.VisualStudio.Services.WebApi.Patch;
+using Microsoft.TeamFoundation.WorkItemTracking.Process.WebApi;
+using Microsoft.TeamFoundation.Core.WebApi;
 
 namespace AzDOUpdateChildField
 {
@@ -30,6 +32,7 @@ namespace AzDOUpdateChildField
             _secretClient = secretClient;
         }
 
+
         [FunctionName(nameof(UpdateChildFieldsWhenParentFieldChanges))]
         public async Task<IActionResult> UpdateChildFieldsWhenParentFieldChanges(
             [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
@@ -39,35 +42,50 @@ namespace AzDOUpdateChildField
             var body = await GetBody<WebhookBody>(req);
 
             using (var workItemConnection = await Connect<WorkItemTrackingHttpClient>())
+            using (var projectConnection = await Connect<ProjectHttpClient>())
+            using (var workItemTrackingConnection = await Connect<WorkItemTrackingProcessHttpClient>())
             {
-                var wi = await workItemConnection.GetWorkItemAsync(body.resource.workItemId, new List<string>() { "Custom.Sciforma", "Custom.StudyName" }, expand: Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models.WorkItemExpand.Relations);
+                var wi = await workItemConnection.GetWorkItemAsync(body.resource.workItemId, new List<string>() { "Custom.Sciforma", "Custom.StudyName", "System.TeamProject" });
+                var wiRelations = await workItemConnection.GetWorkItemAsync(body.resource.workItemId, expand: Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models.WorkItemExpand.Relations);
 
-                foreach (var c in wi.Relations.Where(a => a.Rel == "System.LinkTypes.Hierarchy-Forward"))
+                var project = await projectConnection.GetProject(wi.Fields["System.TeamProject"].ToString());
+
+
+                var processes = await workItemTrackingConnection.GetListOfProcessesAsync(Microsoft.TeamFoundation.WorkItemTracking.Process.WebApi.Models.GetProcessExpandLevel.Projects);
+
+                var projectProcess = processes.FirstOrDefault(a => a.Projects != null && a.Projects.Select(a => a.Id).Contains(project.Id));
+
+                var types = await workItemTrackingConnection.GetProcessWorkItemTypesAsync(projectProcess.TypeId);
+
+                foreach (var c in wiRelations.Relations.Where(a => a.Rel == "System.LinkTypes.Hierarchy-Forward"))
                 {
                     var id = int.Parse(c.Url.Split("/").Last());
-                    var child = await workItemConnection.GetWorkItemAsync(id, new List<string>() { "Custom.Sciforma", "Custom.StudyName" });
-                   
-                    //base work done, need logic to detect if child wi type has custom fields as available options
+                    var child = await workItemConnection.GetWorkItemAsync(id, new List<string>() { "Custom.Sciforma", "Custom.StudyName", "System.WorkItemType" });
+
+                    var fields = await workItemTrackingConnection.GetAllWorkItemTypeFieldsAsync(projectProcess.TypeId,
+                        types.SingleOrDefault(a => a.Name == child.Fields["System.WorkItemType"].ToString()).ReferenceName);
 
                     JsonPatchDocument patch = new JsonPatchDocument();
 
-                    if (wi.Fields["Custom.Sciforma"].ToString() != child.Fields["Custom.Sciforma"].ToString())
+                    if (fields.Select(q => q.ReferenceName).Contains("Custom.Sciforma") && (!child.Fields.ContainsKey("Custom.Sciforma") ||
+                        wi.Fields["Custom.Sciforma"].ToString() != child.Fields["Custom.Sciforma"].ToString()))
                     {
                         patch.Add(new JsonPatchOperation()
                         {
                             Operation = Operation.Add,
                             Path = "/fields/Custom.Sciforma",
-                            Value = wi.Fields["Custom.Sciforma"].ToString()
+                            Value = !wi.Fields.ContainsKey("Custom.Sciforma") ? "" : wi.Fields["Custom.Sciforma"].ToString()
                         });
                     }
 
-                    if (wi.Fields["Custom.StudyName"].ToString() != child.Fields["Custom.StudyName"].ToString())
+                    if (fields.Select(q => q.ReferenceName).Contains("Custom.StudyName") && (!child.Fields.ContainsKey("Custom.StudyName") ||
+                        wi.Fields["Custom.StudyName"].ToString() != child.Fields["Custom.StudyName"].ToString()))
                     {
                         patch.Add(new JsonPatchOperation()
                         {
                             Operation = Operation.Add,
                             Path = "/fields/Custom.StudyName",
-                            Value = wi.Fields["Custom.StudyName"].ToString()
+                            Value = !wi.Fields.ContainsKey("Custom.StudyName") ? "" : wi.Fields["Custom.StudyName"].ToString()
                         });
                     }
 
